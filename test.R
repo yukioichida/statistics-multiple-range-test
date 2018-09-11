171ebe5774fb041fe31f6a958179da03914aecc7
Alexandre Yukio Ichida <yukioichida@gmail.com>
2018-09-11 10:23:54 -0300

    correção teste duncan

--- test.R
+++ test.R
@@ -1,26 +1,4 @@
-chocolate = data.frame(
-  Sabor =
-    c(7, 9, 5,
-      6, 5, 7,
-      7, 6, 7,
-      7, 8, 2,
-      9, 7, 3,
-      4, 9, 2,
-      6, 6, 4,
-      5, 7, 4,
-      6, 5, 5,
-      7, 9, 2,
-      4, 5, 3,
-      5, 7, 4,
-      9, 6, 4,
-      7, 5, 4,
-      8, 6, 3
-    ),
-  Tipo = factor(rep(c("A", "B", "C"), 15)),
-  Provador = factor(rep(1:15, rep(3, 15))))
-write.csv(chocolate, file="chocolate_data.csv",row.names=FALSE)
-
-chocolate_df = read.csv("chocolate_data.csv")
+student_df = read.csv("student_data.csv")
 
 
 ajuste = lm(data = chocolate_df, Sabor ~ .)
